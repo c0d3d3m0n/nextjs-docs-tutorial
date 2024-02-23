60b9c4054407cd714e16260c2f7044325ccdcacf
jameydev <jybryce4@gmail.com>
2024-02-23 15:11:24 -0600

    chapter 16 complete

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Page() {
-    return (
-        <div>
-            <h1>Customers Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Customers Page</h1>
+    </div>
+  );
 }
